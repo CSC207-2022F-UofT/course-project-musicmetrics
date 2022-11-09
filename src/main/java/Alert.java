8c88f53d77462567965a06a3c84224c51fccd39a
amirhosseindavoodi <amirhossein.davoodi@mail.utoronto.ca>
2022-11-08 23:28:35 -0500

    constructor and instance for Alert

--- src/main/java/Alert.java
+++ src/main/java/Alert.java
@@ -1,5 +1,9 @@
 public class Alert {
-    public Alert(){
+    int stream_in_period;
+    static int growth_rate = 100;
+    public Alert(int stream_in_period){
+        this.stream_in_period = stream_in_period;
+
 
     }
     public void trigger(){
