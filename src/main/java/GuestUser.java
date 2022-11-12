d60cb62525b8f633559e3cfdc83e7b0e606f0b95
Yu Yang Tan <tyuyang.tan@gmail.com>
2022-11-11 23:15:06 -0500

    Created new text files with logged-in users and registered users. Updated RegisteredUser, GuestUser and UserData.

--- src/main/java/GuestUser.java
+++ src/main/java/GuestUser.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class GuestUser extends User{
     public GuestUser(){
@@ -12,12 +13,30 @@ public class GuestUser extends User{
         return null;
     }
 
+    public void register() {
+        // calls on userdata to save user
+    }
+
     public void saveUser(){
     }
 
     public void deleteUser(){
     }
 
+    public void logInUser() {
+        UserData u = new UserData();
+        // ask for email and password
+        // call on u.login
+    }
     public void logoutUser(){
+    }
+
+    public void forgotPassword() {
+        // ask for email
+        Scanner emailInput = new Scanner(System.in);
+        System.out.println("Please enter your email address.");
+        // sends an email
+        System.out.println("An email to reset your password has been sent to " + emailInput.next());
+        // email leads to her instance of RegisteredUser
     }
 }
--- src/main/java/RegisteredUser.java
+++ src/main/java/RegisteredUser.java
@@ -2,8 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RegisteredUser extends User{
-    public RegisteredUser() {
+    private final String email;
+    private String password;
+    public ArrayList<String> follows;
 
+    public RegisteredUser(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.follows = new ArrayList<>();
     }
 
     public Alert getAlerts(double growthRate, boolean compare, int top) {
@@ -27,12 +33,21 @@ public class RegisteredUser extends User{
         return true;
     }
 
-    public void logout() {
-
+    public GuestUser logout() {
+        UserData u = new UserData();
+        return u.logoutUser(this.email, this.password);
     }
 
-    public void changePassword() {
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
 
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
     }
 
     public void setFollows() {
