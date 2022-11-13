119ab7bb340eb2958f755c081bf655c42feddbc5
amirhosseindavoodi <amirhossein.davoodi@mail.utoronto.ca>
2022-11-13 15:14:31 -0500

    fixed trigger2

--- src/main/java/Alert.java
+++ src/main/java/Alert.java
@@ -5,30 +5,25 @@ import java.util.List;
 
 public class Alert {
     int stream;
-    int stream_in_period;
-    boolean hit;
     static double growth_rate = 1.25;
     ArrayList<String> tops ;
 
-    public Alert(int stream, int stream_in_period, boolean hit){
-        this.stream = stream;
-        this.stream_in_period = stream_in_period;
+    public Alert(){
         ArrayList<String> tops = new ArrayList<String>();
 
 
     }
-    public void trigger(ArrayList<Artist> artists, int stream, int stream_in_period){
+    public void trigger(ArrayList<Artist> artists){
         for (Artist i: artists){
-            int latest = MusicData.getLatestWeek();
-            List<Integer> weeks = Arrays.asList(latest - 1, latest);
+            List<Integer> weeks = Arrays.asList(MusicData.getLatestWeek() - 1, MusicData.getLatestWeek());
 
             HashMap<Integer, Integer> stream_week = MusicData.getStreams(i, weeks);
-            if(stream_week.get(latest - 1) * growth_rate <= stream_week.get(latest)) {
+            if(stream_week.get(MusicData.getLatestWeek() - 1) * growth_rate <= stream_week.get(MusicData.getLatestWeek())) {
                 this.tops.add(i.getName());
             }
         }
     }
     public void sendAlert(){
-
+        String message = this.tops.get(this.tops.size() - 1) + "is now on fire";
     }
 }
