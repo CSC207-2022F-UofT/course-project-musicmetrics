1785c62caa9c92b1e913da4105dddb58f7f844a6
amirhosseindavoodi <amirhossein.davoodi@mail.utoronto.ca>
2022-11-21 11:14:39 -0500

    added showtops and changed trigger

--- src/main/java/Alert.java
+++ src/main/java/Alert.java
@@ -16,20 +16,33 @@ public class Alert {
 
     /** trigger checks if the artists streams had the sufficient amount of growth compare to its last week
      * streams
-     * @param artists is the list of all artists that in trigger, their streams are compared and the top ones
+     * @param user is the list of all artists that in trigger, their streams are compared and the top ones
      *                will be determined.
      */
-    public void trigger(ArrayList<Artist> artists){
-        for (Artist i: artists){
+    public void trigger(RegisteredUser user){
+        List<Artist> follows = user.getFollows();
+        for (Artist i: follows){
             List<Integer> weeks = Arrays.asList(MusicData.getLatestWeek() - 1, MusicData.getLatestWeek());
 
             HashMap<Integer, Integer> stream_week = MusicData.getStreams(i, weeks);
-            if(stream_week.get(MusicData.getLatestWeek() - 1) * growth_rate <= stream_week.get(MusicData.getLatestWeek())) {
-                this.tops.add(i.getName());
+            if(stream_week.get(MusicData.getLatestWeek() - 1) * growth_rate <=
+                    stream_week.get(MusicData.getLatestWeek())) {
+                if(! this.tops.contains(i.getName())){
+                    this.tops.add(i.getName());
+                }
+
             }
         }
     }
     public void sendAlert(){
         this.message = this.tops.get(this.tops.size() - 1) + "is now on fire";
+    }
+
+    /**
+     * this method returns a list of top artists names
+     * @return
+     */
+    public ArrayList<String> showtops(){
+        return this.tops;
     }
 }
