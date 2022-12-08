9f1cf660791e503e1b4f76248d01cadcf6ce170d
maxXu0713 <max.xu.0713@gmail.com>
2022-12-08 11:48:18 -0500

    testing adjustments based on Follows being strings instead of Artists

--- src/test/java/RecommendArtistTest.java
+++ src/test/java/RecommendArtistTest.java
@@ -18,18 +18,18 @@ public class RecommendArtistTest {
                 //reset data
                 MusicDataBuilder.setData();
 
-                Artist Drake = new Artist(1000, "Hip-Hop/Rap", "Drake",
-                        new Boolean[] {true, false, false, false, true}, 1, 1000);
-                Artist Jcole = new Artist(1000, "Hip-Hop/Rap", "Jcole",
-                        new Boolean[] {true, true, false, false, false}, 1, 1000);
-                Artist Kendrick = new Artist(1000, "Hip-Hop/Rap", "Kendrick",
-                        new Boolean[] {true, true, false, false, true}, 1, 1000);
+                entities.Artist Drake = new entities.Artist(1000, "Hip-Hop/Rap", "Drake",
+                        new Boolean[] {false, false, false, false, false, true}, 1, 1000);
+                entities.Artist Eminem = new entities.Artist(1000, "Hip-Hop/Rap", "Eminem",
+                        new Boolean[] {true, true, false, false, false, false}, 1, 1000);
+                entities.Artist Post = new entities.Artist(1000, "Hip-Hop/Rap", "Post Malone",
+                        new Boolean[] {true, true, false, false, true, false}, 1, 1000);
                 Artist Taylor = new Artist(1000, "Pop", "Taylor Swift",
                         new Boolean[] {true, true, false, false, true}, 1, 1000);
                 ArrayList<Artist> week1Artist = new ArrayList<>();
                 week1Artist.add(Drake);
-                week1Artist.add(Jcole);
-                week1Artist.add(Kendrick);
+                week1Artist.add(Eminem);
+                week1Artist.add(Post);
                 week1Artist.add(Taylor);
                 MusicData.data.put(1, week1Artist);
 
@@ -52,17 +52,17 @@ public class RecommendArtistTest {
                 //follow is within same genre
                 entities.Artist Drake = new entities.Artist(1000, "Hip-Hop/Rap", "Drake",
                         new Boolean[] {false, false, false, false, false, true}, 1, 1000);
-                entities.Artist Jcole = new entities.Artist(1000, "Hip-Hop/Rap", "Jcole",
+                entities.Artist Eminem = new entities.Artist(1000, "Hip-Hop/Rap", "Eminem",
                         new Boolean[] {true, true, false, false, false, false}, 1, 1000);
-                entities.Artist Kendrick = new entities.Artist(1000, "Hip-Hop/Rap", "Kendrick",
+                entities.Artist Post = new entities.Artist(1000, "Hip-Hop/Rap", "Post Malone",
                         new Boolean[] {true, true, false, false, true, false}, 1, 1000);
                 ArrayList<entities.Artist> week1Artist = new ArrayList<>();
                 week1Artist.add(Drake);
-                week1Artist.add(Jcole);
-                week1Artist.add(Kendrick);
+                week1Artist.add(Eminem);
+                week1Artist.add(Post);
                 use_cases.MusicData.data.put(3, week1Artist);
 
-                ArrayList<entities.Artist> userFollows = new ArrayList<>();
+                ArrayList<String> userFollows = new ArrayList<>();
                 rUser.setFollows(userFollows);
 
                 //edge case User Follows No one
@@ -70,11 +70,10 @@ public class RecommendArtistTest {
                 Assertions.assertEquals("Hip-Hop/Rap", reccommendation.getGenre());
 
                 //user follows someone
-                userFollows.add(Kendrick);
+                userFollows.add(Post.getName());
                 rUser.setFollows(userFollows);
-                use_cases.MusicData mD = new use_cases.MusicData();
                 entities.Artist recommendationRap = RecommendArtist.recommend("Hip-Hop/Rap", true, rUser);
-                Assertions.assertEquals("Jcole",recommendationRap.getName());
+                Assertions.assertEquals("Eminem",recommendationRap.getName());
 
                 //follow is within different genre
                 Artist Ariana = new Artist(1000, "Pop", "Ariana Grande",
@@ -88,8 +87,8 @@ public class RecommendArtistTest {
                 Assertions.assertEquals("Taylor Swift", recommendationPop.getName());
 
                 //edge case User Follows everyone
-                userFollows.add(Drake);
-                userFollows.add(Jcole);
+                userFollows.add(Drake.getName());
+                userFollows.add(Eminem.getName());
                 Artist reccommendationEveryone = RecommendArtist.recommend("Hip-Hop/Rap", false, rUser);
                 Assertions.assertEquals("Hip-Hop/Rap", reccommendationEveryone.getGenre());
         }
