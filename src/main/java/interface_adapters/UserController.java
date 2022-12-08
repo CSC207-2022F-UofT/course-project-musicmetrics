ed626a0cb7a5e60a51fcf6915b5b15eeefaa9d7a
jmlee3210 <jmlee321083@gmail.com>
2022-12-07 21:54:32 -0500

    Added documentation.

--- src/main/java/interface_adapters/UserController.java
+++ src/main/java/interface_adapters/UserController.java
@@ -2,18 +2,38 @@ package interface_adapters;
 import use_cases.*;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class UserController {
 
+    /**
+     * Let logged-in user (currentUser) in UserData to follow the given artist
+     *
+     * @param userData UserData
+     * @param email logged-in user's email
+     * @param artist Artist name to follow
+     */
     public static void followArtist(UserData userData, String email, String artist) throws Exception {
         userData.getUser(email).followArtist(MusicData.getArtistByName(artist));
     }
 
+    /**
+     * Let logged-in user (currentUser) in UserData to unfollow the given artist
+     *
+     * @param userData UserData
+     * @param email logged-in user's email
+     * @param artist Artist name to unfollow
+     */
     public static void unfollowArtist(UserData userData, String email, String artist) throws Exception {
         userData.getUser(email).unfollowArtist(MusicData.getArtistByName(artist));
     }
 
+    /**
+     * Returns an ArrayList of Artist name that the logged-in user in userData has followed
+     *
+     * @param userData UserData
+     * @param email logged-in user's email
+     * @return an ArrayList of followed Artist name
+     */
     public static List<String> getFollowedArtistNames(UserData userData, String email) throws Exception {
         return ArtistComparer.artistToNames(userData.getUser(email).getFollows());
     }
--- src/main/java/use_cases/ArtistComparer.java
+++ src/main/java/use_cases/ArtistComparer.java
@@ -35,6 +35,14 @@ public class ArtistComparer {
         return artistToNames(use_cases.MusicData.getTop(use_cases.MusicData.getLatestWeek(), amt));
     }
 
+    /**
+     * Returns an ArrayList of trending Artist names based on given parameters.
+     *
+     * @param startWeek the starting week number
+     * @param endWeek the ending week number
+     * @param amt the number of artists names needed
+     * @return an ArrayList of trending Artists names
+     */
     public static List<String> trendingArtistNames(int startWeek, int endWeek, int amt) {
         List<Artist> artists = MusicData.getTrending(startWeek, endWeek);
         List<String> names = new ArrayList<>();
@@ -49,6 +57,12 @@ public class ArtistComparer {
         return names;
     }
 
+    /**
+     * Returns an ArrayList of Artist name given a List of Artist
+     *
+     * @param artists a List of Artist to convert to an ArrayList of names
+     * @return an ArrayList of Artists names
+     */
     public static List<String> artistToNames(List<Artist> artists) {
         List<String> names = new ArrayList<>();
         for (Artist artist : artists) {
